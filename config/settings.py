6b8d6d97ab51a7d40595b48794114a28ecdee014
superdev0816 <133602290+superdev0816@users.noreply.github.com>
2024-01-15 00:15:06 +0100

    Update settings.py
    tmp

--- config/settings.py
+++ config/settings.py
@@ -137,11 +137,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/admin"
 
 
-
-load_dotenv()
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'wealthyonlinecoaching@gmail.com' 
+EMAIL_HOST_PASSWORD = 'shyyspsvqyectbwe'
+
+#load_dotenv()
+#EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+#EMAIL_HOST = 'smtp.gmail.com' 
+#EMAIL_PORT = 587 
+#EMAIL_USE_TLS = True 
