c6755ecbdb29952c0c820993054583d321ec6e96
superdev0816 <133602290+superdev0816@users.noreply.github.com>
2024-01-14 23:30:38 +0100

    Update settings.py
    fixed environ

--- config/settings.py
+++ config/settings.py
@@ -139,8 +139,8 @@ LOGIN_URL = "/admin"
 
 
 load_dotenv()
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
