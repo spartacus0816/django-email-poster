1e62be85f3d829e5ea72a82261dea8f4ae3dca39
superdev0816 <133602290+superdev0816@users.noreply.github.com>
2024-01-15 02:26:29 +0100

    Update models.py

--- main/models.py
+++ main/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Client(models.Model):
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
+    wealthy_online_coaching_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=155, unique=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField(max_length=254, blank=True, null=True)
